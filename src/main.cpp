f262beb80f1a052b053e07bd4290563daf2f67af
detroidSandwich <detroidsandwich@gmail.com>
2025-06-07 14:47:51 +0200

    Empty project

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,32 @@
 #include <Esp.h>
-#include "Led.h"
-#include "Web.h"
-#include "DhtManager.h"
+#include <Arduino.h>
+#include <FastLED.h>
 
-Led led;
-Web web;
-DhtManager dht;
+// --- Настройки светодиодов ---
+#define LED_PIN     D2        // Пин, к которому подключены данные светодиодной ленты (например, D2 на NodeMCU)
+#define NUM_LEDS    256       // Количество светодиодов в матрице 16x16 (16 * 16 = 256)
+#define BRIGHTNESS  64        // Начальная яркость (от 0 до 255)
+#define COLOR_ORDER GRB       // Порядок цветов (GRB для большинства матриц)
+#define LED_TYPE    WS2812B   // Тип светодиодов (например, WS2812B)
 
-void setup()
-{
-  LedData ledData;
-  Serial.begin(115200);
-  led.setup(&ledData);
-  web.setup(ledData, [](LedData data)
-            { led.updateData(&data); });
-  dht.setup([](float temp, float hum)
-            { web.updateDht(temp, hum); });
-};
+// Определяем массив для хранения информации о каждом светодиоде
+CRGB leds[NUM_LEDS];
 
-void loop()
-{
-  uint32_t ms = millis();
-  led.update(ms);
-  web.update();
-  dht.update(ms, [](float temp, float hum)
-             { web.updateDht(temp, hum); });
-};
+void setup() {
+  Serial.begin(115200); // Инициализация последовательного порта для отладки
+  Serial.println("Starting GyverLampWeb...");
+
+  // Инициализация FastLED
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+
+  // Устанавливаем все светодиоды в красный цвет для теста
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Red;
+  }
+  FastLED.show(); // Отправляем данные на светодиоды
+}
+
+void loop() {
+  // В loop пока ничего не делаем, просто держим светодиоды красными
+}
